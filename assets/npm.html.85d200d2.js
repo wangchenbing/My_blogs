5a57b6d20ec529fb7531309762bf5550edf38ec7
王晨冰 <wcb_houhuiyouqi@126.com>
2022-12-08 22:04:56 +0800

    deploy

---
+++ assets/npm.html.85d200d2.js
@@ -0,0 +1,29 @@
+import{_ as s}from"./_plugin-vue_export-helper.cdc0426e.js";import{o as n,c as e,d as a}from"./app.492b5a57.js";const r={},p=a(`<div class="language-javascript ext-js line-numbers-mode"><pre class="language-javascript"><code><span class="token comment">// \u67E5\u770Bnpm\u5F53\u524D\u955C\u50CF\u6E90</span>
+npm config <span class="token keyword">get</span> registry  
+
+<span class="token comment">// \u8BBE\u7F6Enpm\u955C\u50CF\u6E90\u4E3A\u6DD8\u5B9D\u955C\u50CF</span>
+npm config <span class="token keyword">set</span> registry https<span class="token operator">:</span><span class="token operator">/</span><span class="token regex"><span class="token regex-delimiter">/</span><span class="token regex-source language-regex">registry.npm.taobao.org</span><span class="token regex-delimiter">/</span></span>  
+
+<span class="token comment">//\u6E05\u9664npm\u7F13\u5B58 </span>
+npm cache clean <span class="token operator">--</span>force
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="\u955C\u50CF\u6E90\u5730\u5740" tabindex="-1"><a class="header-anchor" href="#\u955C\u50CF\u6E90\u5730\u5740" aria-hidden="true">#</a> \u955C\u50CF\u6E90\u5730\u5740</h2><div class="language-javascript ext-js line-numbers-mode"><pre class="language-javascript"><code>npm <span class="token operator">--</span><span class="token operator">-</span> https<span class="token operator">:</span><span class="token operator">/</span><span class="token regex"><span class="token regex-delimiter">/</span><span class="token regex-source language-regex">registry.npmjs.org</span><span class="token regex-delimiter">/</span></span>
+
+cnpm <span class="token operator">--</span><span class="token operator">-</span> https<span class="token operator">:</span><span class="token operator">/</span><span class="token regex"><span class="token regex-delimiter">/</span><span class="token regex-source language-regex">r.cnpmjs.org</span><span class="token regex-delimiter">/</span></span>
+
+taobao <span class="token operator">--</span><span class="token operator">-</span> https<span class="token operator">:</span><span class="token operator">/</span><span class="token regex"><span class="token regex-delimiter">/</span><span class="token regex-source language-regex">registry.npm.taobao.org</span><span class="token regex-delimiter">/</span></span>
+
+nj <span class="token operator">--</span><span class="token operator">-</span> https<span class="token operator">:</span><span class="token operator">/</span><span class="token regex"><span class="token regex-delimiter">/</span><span class="token regex-source language-regex">registry.nodejitsu.com</span><span class="token regex-delimiter">/</span></span>
+
+rednpm <span class="token operator">--</span><span class="token operator">-</span> https<span class="token operator">:</span><span class="token operator">/</span><span class="token regex"><span class="token regex-delimiter">/</span><span class="token regex-source language-regex">registry.mirror.cqupt.edu.cn</span><span class="token regex-delimiter">/</span></span>
+
+npmMirror <span class="token operator">--</span><span class="token operator">-</span> https<span class="token operator">:</span><span class="token operator">/</span><span class="token operator">/</span>skimdb<span class="token punctuation">.</span>npmjs<span class="token punctuation">.</span>com<span class="token operator">/</span>registry<span class="token operator">/</span>
+
+deunpm <span class="token operator">--</span><span class="token operator">-</span> http<span class="token operator">:</span><span class="token operator">/</span><span class="token regex"><span class="token regex-delimiter">/</span><span class="token regex-source language-regex">registry.enpmjs.org</span><span class="token regex-delimiter">/</span></span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="\u767B\u5F55npm" tabindex="-1"><a class="header-anchor" href="#\u767B\u5F55npm" aria-hidden="true">#</a> \u767B\u5F55npm</h2><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>  npm login 
+
+  wangchenbing-js
+
+  Houhuiyouqi1258
+
+  wcb_houhuiyouqi@163.com
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,5),o=[p];function t(i,l){return n(),e("div",null,o)}const m=s(r,[["render",t],["__file","npm.html.vue"]]);export{m as default};
